933824eeb5691a9c7c80354c71dc1775a194aa8b
AnikSq <anikr23456@gmail.com>
2021-12-15 08:50:54 +0530

    final'

--- script.js
+++ script.js
@@ -1,90 +1,62 @@
-let value = 0 ;
-let playerWin = 0;
-let computerWin = 0;
-let playerWeapon = "";
-let computerChoice = "";
-let humanCounter = document.getElementById('counter-1')
-let aiCounter = document.getElementById('counter-2')
 
-
-function randomNumber() { 
-
-    value =  Math.floor(Math.random() * (4 - 1) + 1 );
-}
+const game = () => {
 
 
 
+    let value = 0 ;
+    let playerWin = 0;
+    let computerWin = 0;
+    let humanCounter = document.getElementById('counter-1');
+    let aiCounter = document.getElementById('counter-2');
+    let buttonRock = document.querySelector('.rock');
+    let buttonPaper = document.querySelector('.paper');
+    let buttonScissors = document.querySelector('.scissors')
+    let gameEnd = false
+    let computer = ""
 
-function computerPlay(){
 
 
-    
-    randomNumber()
+    function randomNumber() { 
 
-
-    switch(value){
-
-        case 1:
-            computerChoice = "Rock";
-            break;
-        case 2:
-            computerChoice = "Paper";
-            break;
-        case 3:
-            computerChoice = "Scissors";
-            break;
+        value =  Math.floor(Math.random() * (4 - 1) + 1 );
     }
 
+   
+
+
+
+
+    function computerPlay(){
+
+
+        
+        randomNumber()
+
+
+        switch(value){
+
+            case 1:
+                computer =  "rock"
+                break;
+            case 2:
+                computer = "paper"
+                break;
+            case 3:
+                computer = "scissors"
+                break;
+        }
+
 }
 
-function increment(counter){
-    let number = counter.innerHTML
-    number++;
-    counter.innerHTML = number
-}
-
-function playerChoice(){
-
-    playerWeapon = prompt("Please enter your weapon")
-}
+    function increment(counter){
+        let number = counter.innerHTML
+        number++;
+        counter.innerHTML = number
+    }
 
 
-function Game(){
-    
 
-    let gameEnd = false
-
-    while(gameEnd == false){
-    
-        computerPlay()
-        playerChoice()
-        playerWeapon = playerWeapon.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
-
-        if(playerWeapon === computerChoice){
-            alert("A.I : Human be unique")
-        }
-        else if (playerWeapon == "rock" && computerChoice == "scissors"){
-            alert("Fatality , You win this round");
-            playerWin++;
-            increment(humanCounter);
-        }
-        else if (playerWeapon == "scissors" && computerChoice == "paper"){
-            alert("On a way to Victory, Human wins this round")
-            playerWin++;
-            increment(humanCounter);
-        }
-        else if (playerWeapon == "paper" && computerChoice == "rock"){
-            alert("Human Wins")
-            playerWin++;
-            increment(humanCounter);
-        }
-        else{
-            alert("A,I is taking over humans")
-            computerWin++;
-            increment(aiCounter);
-        }
-
+    function gameOver(){
         if (playerWin >= 5){
             alert("Human wins! , keeping civilization alive");
             let answer = prompt("Would you like to play again?")
@@ -108,7 +80,69 @@ function Game(){
         }
     }
     
+
+    
+
+    function playerChoice(){
+
+        
+        buttonRock.addEventListener('click',() => {
+          gameOver();
+          computerPlay();
+            if (computer == "scissors"){
+                playerWin++;
+                increment(humanCounter);
+            }
+            else if(computer == "rock"){
+                alert("Human be unique")
+            }
+            else{
+                alert("A.I is taking over humans")
+                computerWin++;
+                increment(aiCounter);
+            }
+        })
+
+        buttonPaper.addEventListener('click',() => {
+          gameOver();
+          computerPlay();
+            if (computer == "rock"){
+                playerWin++;
+                increment(humanCounter);
+            }
+            else if(computer == "paper"){
+                alert("human be unique")
+            }
+            else{
+                alert("A.I is taking over humans")
+                computerWin++;
+                increment(aiCounter);
+            }
+        })
+
+        buttonScissors.addEventListener('click',() => {
+          gameOver();
+          computerPlay();
+            if (computer == "rock"){
+                playerWin++;
+                increment(humanCounter);
+            }
+            else if(computer == "scissors"){
+                alert("Human be unique")
+            }
+            else{
+                alert("A.I is taking over humans")
+                computerWin++;
+                increment(aiCounter);
+            }
+        })
+    }
+
+    playerChoice();
+
+ 
+
 }
 
-Game()
 
+game();
